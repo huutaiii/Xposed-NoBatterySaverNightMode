c44458d4ad9eb10620c74dcd4d3622f0aaeefca0
huutai <truongtai137@gmail.com>
2021-02-11 14:23:03 +0700

    Dark theme toggle in Settings app.

--- app/src/main/java/ht/android/xposed/nobatterysavernightmode/Module.kt
+++ app/src/main/java/ht/android/xposed/nobatterysavernightmode/Module.kt
@@ -3,10 +3,7 @@
 
 package ht.android.xposed.nobatterysavernightmode
 
-import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedBridge
-import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.*
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class Module : IXposedHookLoadPackage {
@@ -14,48 +11,73 @@ class Module : IXposedHookLoadPackage {
         when (lpparam.packageName) {
             "android" -> {
                 XposedHelpers.findAndHookMethod(
-                    "com.android.server.UiModeManagerService",
-                    lpparam.classLoader,
-                    "updateConfigurationLocked",
-                    object : XC_MethodHook() {
-                        override fun beforeHookedMethod(param: MethodHookParam) {
-                            super.beforeHookedMethod(param)
-                            XposedHelpers.setBooleanField(param.thisObject, "mPowerSave", false)
-                            XposedBridge.log("Hooked method \"${param.method.toString()}\" was called")
-                        }
-                    })
+                        "com.android.server.UiModeManagerService",
+                        lpparam.classLoader,
+                        "updateConfigurationLocked",
+                        object : XC_MethodHook() {
+                            override fun beforeHookedMethod(param: MethodHookParam) {
+                                super.beforeHookedMethod(param)
+                                XposedHelpers.setBooleanField(param.thisObject, "mPowerSave", false)
+                                XposedBridge.log("Hooked method \"${param.method.toString()}\" was called")
+                            }
+                        })
             }
             "com.android.settings" -> {
                 XposedHelpers.findAndHookMethod(
-                    "com.android.settings.display.darkmode.DarkModeObserver",
-                    lpparam.classLoader,
-                    "subscribe",
-                    Runnable::class.java,
-                    object : XC_MethodHook() {
-                        override fun beforeHookedMethod(param: MethodHookParam) {
-                            super.beforeHookedMethod(param)
-                            param.result = null
-                        }
-                    })
+                        "com.android.settings.display.darkmode.DarkModeObserver",
+                        lpparam.classLoader,
+                        "subscribe",
+                        Runnable::class.java,
+                        object : XC_MethodHook() {
+                            override fun beforeHookedMethod(param: MethodHookParam) {
+                                super.beforeHookedMethod(param)
+                                param.result = null
+                            }
+                        })
+                XposedHelpers.findAndHookMethod(
+                        "com.android.settings.display.DarkUIPreferenceController",
+                        lpparam.classLoader, "isPowerSaveMode",
+                        object : XC_MethodReplacement() {
+                            override fun replaceHookedMethod(param: MethodHookParam): Any {
+                                return false
+                            }
+                        })
+//                XposedHelpers.findAndHookMethod(
+//                        "com.android.settings.display.darkmode.DarkModeActivationPreferenceController",
+//                        lpparam.classLoader,
+//                        "updateState",
+//                        object : XC_MethodHook() {
+//                            override fun afterHookedMethod(param: MethodHookParam) {
+//                                super.afterHookedMethod(param)
+//                                val mPowerManager = XposedHelpers.getObjectField(param.thisObject, "mPowerManager")
+//                                val batterySaver = XposedHelpers.callMethod(mPowerManager, "isPowerSaveMode") as Boolean
+//                                if (batterySaver) {
+//                                    val context = XposedHelpers.getObjectField(param.thisObject, "mContext") as Context
+//                                    val active = context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_YES != 0
+//                                    XposedHelpers.callMethod(param.thisObject, "updateNightMode", active)
+//                                }
+//                            }
+//                        }
+//                )
             }
             "com.android.systemui" -> {
                 XposedHelpers.findAndHookMethod(
-                    "com.android.systemui.qs.tiles.UiModeNightTile",
-                    lpparam.classLoader,
-                    "handleUpdateState",
-                    XposedHelpers.findClass("com.android.systemui.plugins.qs.QSTile.BooleanState", lpparam.classLoader),
-                    Object::class.java,
-                    object : XC_MethodHook() {
-                        override fun afterHookedMethod(param: MethodHookParam) {
-                            super.afterHookedMethod(param)
-                            val state = param.args[0]
-                            val classTile = XposedHelpers.findClass("android.service.quicksettings.Tile", lpparam.classLoader)
-                            val value = XposedHelpers.getBooleanField(state, "value")
-                            val active = XposedHelpers.getStaticIntField(classTile, "STATE_ACTIVE")
-                            val inactive = XposedHelpers.getStaticIntField(classTile, "STATE_INACTIVE")
-                            XposedHelpers.setIntField(state, "state", if (value) active else inactive)
+                        "com.android.systemui.qs.tiles.UiModeNightTile",
+                        lpparam.classLoader,
+                        "handleUpdateState",
+                        XposedHelpers.findClass("com.android.systemui.plugins.qs.QSTile.BooleanState", lpparam.classLoader),
+                        Object::class.java,
+                        object : XC_MethodHook() {
+                            override fun afterHookedMethod(param: MethodHookParam) {
+                                super.afterHookedMethod(param)
+                                val state = param.args[0]
+                                val classTile = XposedHelpers.findClass("android.service.quicksettings.Tile", lpparam.classLoader)
+                                val value = XposedHelpers.getBooleanField(state, "value")
+                                val active = XposedHelpers.getStaticIntField(classTile, "STATE_ACTIVE")
+                                val inactive = XposedHelpers.getStaticIntField(classTile, "STATE_INACTIVE")
+                                XposedHelpers.setIntField(state, "state", if (value) active else inactive)
+                            }
                         }
-                    }
                 )
             }
         }
